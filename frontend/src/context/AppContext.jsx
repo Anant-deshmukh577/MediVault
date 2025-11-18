5d0423039514c0e0406eb98a87e2294fa0138c70
Anant <deshmukhanant26@gmail.com>
2025-11-18 14:14:15 +0530

    URL change

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -6,7 +6,7 @@ export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
   const currencySymbol = "₹";
-  const backendUrl = https://medivault-backend-beq1.onrender.com;
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
   const [doctors, setDoctors] = useState([]);
   const [token, setToken] = useState(
