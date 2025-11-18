6b6e686123d2d9d20d0223256eed47b1cd160dd6
Anant Deshmukh <deshmukhanant26@gmail.com>
2025-11-18 12:54:42 +0530

    Update AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -6,7 +6,7 @@ export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
   const currencySymbol = "₹";
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const backendUrl = https://medivault-backend-beq1.onrender.comL;
 
   const [doctors, setDoctors] = useState([]);
   const [token, setToken] = useState(
