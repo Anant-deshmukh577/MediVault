277d00f7d52f39f99c31e296b815c9510351d4e5
Anant Deshmukh <deshmukhanant26@gmail.com>
2025-11-18 12:56:32 +0530

    Update AdminContext.jsx

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -12,7 +12,7 @@ const AdminContextProvider = (props) => {
   const [appointments, setAppointments] = useState([]);
   const [dashData, setDashData] = useState(false);
 
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const backendUrl = https://medivault-backend-beq1.onrender.com;
 
   const getAllDoctors = useCallback(async () => {
     if (!aToken) {
